6d4831fe27bae2bca56e2ff06b26c759cf40bda0
rageNami <gr7p4zxh@gmail.com>
2017-11-09 01:13:41 +0800

    fix rekt threshold

--- BitMEX/GroupLiquidationNotice.js
+++ BitMEX/GroupLiquidationNotice.js
@@ -25,7 +25,7 @@ setInterval(() => {
 
         // 限定XBT且>3*10^5的爆倉資訊
         let arr = wsc.liquidation.filter((data) => {
-            return data.symbol.includes('XBT') && (Math.log(data.leavesQty) / Math.log(10) > 0.47712125472);
+            return data.symbol.includes('XBT') && (Math.log(data.leavesQty) / Math.log(10) > 5.47712125472);
         });
 
         let replyMsg = '',
